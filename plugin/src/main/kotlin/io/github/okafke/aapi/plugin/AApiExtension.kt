2aaa785cc30467a62c94d2f1a74189617029eaa4
okafke <65917827+okafke@users.noreply.github.com>
2023-05-06 11:35:45 +0200

    Fix plugin 1.0.3

--- plugin/src/main/kotlin/io/github/okafke/aapi/plugin/AApiExtension.kt
+++ plugin/src/main/kotlin/io/github/okafke/aapi/plugin/AApiExtension.kt
@@ -2,12 +2,15 @@ package io.github.okafke.aapi.plugin
 
 import org.gradle.api.Project
 import org.gradle.api.file.DirectoryProperty
+import org.gradle.api.model.ObjectFactory
 import java.io.File
+import javax.inject.Inject
 
-interface AApiExtension {
-    val dir: DirectoryProperty
-
-    val cacheDir: DirectoryProperty
+open class AApiExtension @Inject constructor(
+    objectFactory: ObjectFactory
+) {
+    private val dir: DirectoryProperty = objectFactory.directoryProperty()
+    private val cacheDir: DirectoryProperty = objectFactory.directoryProperty()
 
     fun getDir(project: Project): File {
         return dir.map { d -> d.asFile }.orElse(File(project.rootProject.rootDir, "build/aapi-tree")).get()
